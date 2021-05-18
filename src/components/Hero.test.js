30dc86a9470e0f56512f32812d2075075d832f52
SAnjosMatheus <matheus.msa07@gmail.com>
2021-05-17 22:23:55 -0300

    Add Hero component

---
+++ src/components/Hero.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Hero from "./Hero";
+
+test("renders Hero with children", () => {
+  const { getByText } = render(
+    <Hero>
+      <p>Matheus Anjos</p>
+    </Hero>
+  );
+
+  expect(getByText("Matheus Anjos")).toBeInTheDocument();
+});
+
+test("renders image background", () => {
+  const image = "http://test/image.jpg";
+
+  const { getByTestId } = render(<Hero image={image} />);
+
+  expect(getByTestId("hero")).toHaveStyleRule({
+    backgroundImage: image,
+  });
+});
